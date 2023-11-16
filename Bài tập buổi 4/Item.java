09043ad7e8cfbeeafa4436444f89cb18c69a2cb7
Bùi Thế Vĩnh Nguyên <118871933+NguyenBui256@users.noreply.github.com>
2023-11-16 22:22:07 +0700

    Add files via upload

---
+++ Bài tập buổi 4/Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    protected String ten, giaBan, thuongHieu;
+
+    public void show() {
+
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public String getGiaBan() {
+        return giaBan;
+    }
+
+    public String getThuongHieu() {
+        return thuongHieu;
+    }
+
+}
---
+++ Bài tập buổi 4/Pencil.java
@@ -0,0 +1,53 @@
+public class Pencil extends Item{
+
+    static int soLuong = 0;
+    private String mauSac, chatLieu, doCung;
+
+    public Pencil(String ten, String giaBan, String thuongHieu, String mauSac, String chatLieu, String doCung) {
+        this.ten = ten;
+        this.giaBan = giaBan;
+        this.thuongHieu = thuongHieu;
+        this.mauSac = mauSac;
+        this.chatLieu = chatLieu;
+        this.doCung = doCung;
+        soLuong += 1;
+    }
+    @Override
+    public void show()
+    {
+        System.out.printf("Tên sản phẩm: %s\n", this.getTen());
+        System.out.printf("Giá bán: %s\n", this.getGiaBan());
+        System.out.printf("Thương hiệu: %s\n", this.getThuongHieu());
+        System.out.printf("Màu sắc: %s\n", this.getMauSac());
+        System.out.printf("Chất liệu: %s\n", this.getChatLieu());
+        System.out.printf("Độ cứng: %s\n", this.getDoCung());
+    }
+
+    public static int getSoLuong() {
+        return soLuong;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public String getGiaBan() {
+        return giaBan;
+    }
+
+    public String getThuongHieu() {
+        return thuongHieu;
+    }
+
+    public String getMauSac() {
+        return mauSac;
+    }
+
+    public String getChatLieu() {
+        return chatLieu;
+    }
+
+    public String getDoCung() {
+        return doCung;
+    }
+}
